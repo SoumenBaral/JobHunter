4661ecca4dadb6282ac475e95e5050e2fa17b205
SoumenBaral <shumen.sb@gmail.com>
2023-12-20 15:41:34 +0600

    added

--- jobApp/urls.py
+++ jobApp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home,name='home'),
-    path('job/<int:id>',views.jobDetails)
+    path('job/<int:id>',views.jobDetails,name='jobDetail')
 ]
