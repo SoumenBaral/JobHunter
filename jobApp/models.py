b1ee72ace928017d7e7dfcda4fc32259a3aa7658
SoumenBaral <shumen.sb@gmail.com>
2023-12-22 02:55:36 +0600

    new project

--- jobApp/models.py
+++ jobApp/models.py
@@ -1,5 +1,6 @@
+from collections.abc import Iterable
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 
 class jobPost(models.Model):
@@ -7,6 +8,12 @@ class jobPost(models.Model):
     description = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
     salary = models.IntegerField()
+    slug = models.SlugField(null=True,max_length= 40,unique = True)
+
+    def save(self, *args, **kwargs):
+        # if not self.id:
+        self.slug = slugify(self.title)
+        return super(jobPost,self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
--- jobApp/views.py
+++ jobApp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseNotFound
+from . import models 
 def home (request):
     temp = tempClass
     is_authentication = False
-    context = {'data':jobTitle,'temp': temp,'is_authentication': is_authentication}
-    return  render(request,'hello.html',context)
+    # context = {'data':jobTitle,'temp': temp,'is_authentication': is_authentication}
+    context = models.jobPost.objects.all()
+    return  render(request,'hello.html',{'context':context})
+
 
 class tempClass :
     x = 6
@@ -23,7 +26,8 @@ jobDescription = [
 
 def jobDetails(request,id):
     try:
-        return_html = f"<h3>{jobTitle[id]}</h3> <p>{jobDescription[id]}</p>"
-        return HttpResponse(return_html)
+        job = models.jobPost.objects.get(id=id)
+        
+        return render(request,'jobdetails.html',{'job':job})
     except:
         return HttpResponseNotFound('Page Not Found')
