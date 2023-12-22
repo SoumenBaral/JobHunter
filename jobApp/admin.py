6c52cab606db853bb8556924e936565c413cbea7
SoumenBaral <shumen.sb@gmail.com>
2023-12-22 19:10:53 +0600

    add a new model

--- jobApp/admin.py
+++ jobApp/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from  . import models
 
 # Register your models here.
+class jobAdmin(admin.ModelAdmin):
+    list_display = ('__str__','title','date','salary')
+    list_filter = ('date','salary')
+    # search_fields = ['title']
+    search_fields = ('title',)
+    search_help_text = 'Search here to find you wanted Job'
+    # fields = ('title',('description','expire'))
+    # exclude = ('title',)
+    fieldsets = (
+        ("Basic Information", {
+            "fields": (
+                'title','description'
+            ),
+        }),
+        ("More  Information", {
+            "classes":('collapse',),
+            "fields": (
+                ('expire','salary'),'slug'
+            ),
+        }),
+        
+    )
+#    'wide' 
 
-admin.site.register(models.jobPost)
+admin.site.register(models.jobPost,jobAdmin)
--- jobApp/models.py
+++ jobApp/models.py
@@ -2,13 +2,27 @@ from collections.abc import Iterable
 from django.db import models
 from django.utils.text import slugify
 # Create your models here.
+class Location(models.Model):
+    street =  models.CharField(max_length =200)
+    city  = models.CharField(max_length =200)
+    state = models.CharField(max_length =200)
+    country = models.CharField(max_length =200)
+    zip = models.CharField(max_length =200)
+
+class Author(models.Model):
+    name = models.CharField(max_length =200)
+    company = models.CharField(max_length =200)
+    designation = models.CharField(max_length =200)
 
 class jobPost(models.Model):
     title = models.CharField(max_length= 200)
     description = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
+    expire = models.DateField(null = True)
     salary = models.IntegerField()
     slug = models.SlugField(null=True,max_length= 40,unique = True)
+    location = models.OneToOneField(Location,null = True ,on_delete = models.CASCADE)
+    author = models.ForeignKey(Author,on_delete = models.CASCADE)
 
     def save(self, *args, **kwargs):
         # if not self.id:
@@ -16,5 +30,6 @@ class jobPost(models.Model):
         return super(jobPost,self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.title
+        return f'{self.title} Company in SX'
     
+
